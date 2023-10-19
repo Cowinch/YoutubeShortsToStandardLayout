73a282f06c59d167476d7db159265840ba45188b
Cole Winchester <cowinch@gmail.com>
2023-10-18 21:19:09 -0500

    CSS updated

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const button = document.createElement('button')
-const container = document.getElementById('content')
-button.textContent = 'change view'
-button.style.position='absolute'
-button.style.right='10%'
-button.style.top='10%'
-container.appendChild(button)
+const icon = document.createElement('img')
+const container = document.getElementById('actions')
+
+icon.src = 'https://i.imgur.com/S6StmBo.png'
+button.style.color="white"
+button.style.borderRadius='50%'
+button.style.height= button.style.width = '48px'
+button.style.backgroundColor='#272727'
+button.style.border = 'none'
+button.style.cursor='pointer'
+if(container){
+    container.appendChild(button)
+    button.style.paddingTop = "16px"
+} else {
+    document.body.appendChild(button)
+    button.style.position='absolute'
+    button.style.right='10%'
+    button.style.top='10%'
+}
+button.appendChild(icon)
 
 button.addEventListener('click', () => {
     location.href = `https://www.youtube.com/watch?v=${location.href.split('/').pop()}`
